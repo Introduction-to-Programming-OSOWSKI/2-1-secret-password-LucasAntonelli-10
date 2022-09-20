ecef3c9a67b0d91aa477801b9a0a37a4c9a6935b
LucasAntonelli-10 <818800@mystma.org>
2022-09-20 15:02:40 -0500

    2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def password(p):
     if p == "Knights19":
-        return "access granted"
+        return "ACCESS GRANTED"
     else:
-        return "access denied"
+        return "ACCESS DENIED"
